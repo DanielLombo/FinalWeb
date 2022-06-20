2631975d90b3959d0952a3b439c7ec99f5d7e902
DanielLombo <danielxviper6314@gmail.com>
2022-06-20 10:27:35 +0800

    project_Hacktiv

---
+++ Portofolio.js
@@ -0,0 +1,53 @@
+function myFunctionHome(){
+    document.getElementById("home").style.display = "block";
+    document.getElementById("about").style.display = "none";
+    document.getElementById("porto").style.display = "none";
+    document.getElementById("blog").style.display = "none";
+    document.getElementById("team").style.display = "none";
+    document.getElementById("contact").style.display = "none";
+}
+
+function myFunctionAbout(){
+    document.getElementById("about").style.display = "block";
+    document.getElementById("home").style.display = "none";
+    document.getElementById("porto").style.display = "none";
+    document.getElementById("blog").style.display = "none";
+    document.getElementById("team").style.display = "none";
+    document.getElementById("contact").style.display = "none";
+}
+
+function myFunctionPorto(){
+    document.getElementById("porto").style.display = "block"
+    document.getElementById("about").style.display = "none";
+    document.getElementById("home").style.display = "none";
+    document.getElementById("blog").style.display = "none";
+    document.getElementById("team").style.display = "none";
+    document.getElementById("contact").style.display = "none";
+}
+
+function myFunctionBlog(){
+    document.getElementById("blog").style.display = "block"
+    document.getElementById("about").style.display = "none";
+    document.getElementById("home").style.display = "none";
+    document.getElementById("porto").style.display = "none";
+    document.getElementById("team").style.display = "none";
+    document.getElementById("contact").style.display = "none";
+}
+
+function myFunctionTeam(){
+    document.getElementById("team").style.display = "block"
+    document.getElementById("about").style.display = "none";
+    document.getElementById("home").style.display = "none";
+    document.getElementById("porto").style.display = "none";
+    document.getElementById("blog").style.display = "none";
+    document.getElementById("contact").style.display = "none";
+}
+
+function myFunctionContact(){
+    document.getElementById("contact").style.display = "block"
+    document.getElementById("about").style.display = "none";
+    document.getElementById("home").style.display = "none";
+    document.getElementById("porto").style.display = "none";
+    document.getElementById("blog").style.display = "none";
+    document.getElementById("team").style.display = "none";
+}
